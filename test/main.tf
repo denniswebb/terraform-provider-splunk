5a73be4e3fabbc0ca36e4a8f43bb81eb4e124261
Dennis Webb <dennis@bluesentryit.com>
2018-02-24 12:57:04 -0600

    adds support for acls (#6)
    * adds support for acls
    
    * go fmt

--- test/main.tf
+++ test/main.tf
@@ -4,22 +4,42 @@ provider splunk {
 }
 
 resource "splunk_saved_search" "test1" {
-  name   = "dennis's test now with ++ signs"
-  search = "kubernetes.labels.app=sisyphus index=dev"
+  name   = "dennis's test"
+  search = "what"
+
+  acl {
+    owner = "terraform"
+    sharing = "global"
+    read = ["*"]
+    write= ["admin","user"]
+  }
 }
 
-resource "splunk_saved_search" "test2" {
-  name                       = "dennis's test alarm"
-  search                     = "sisyphus"
-  actions                    = "slack"
-  action_email_to            = "dennis@bluesentryit.com"
-  action_email_subject       = "Test Alert"
-  action_email_message_alert = "Hey is this thing on!"
-  action_slack_channel       = "@dennis"
-  action_slack_message       = "Hey dude!"
-  alert_comparator           = "greater than"
-  alert_threshold            = "1"
-  dispatch_earliest_time     = "-15m"
-  cron_schedule              = "*/5 * * * *"
-  is_scheduled               = true
-}
+// resource "splunk_saved_search" "test2" {
+//   name                       = "dennis's test alarm"
+//   search                     = "sisyphus"
+//   actions                    = "slack"
+//   action_email_to            = "dennis@bluesentryit.com"
+//   action_email_subject       = "Test Alert"
+//   action_email_message_alert = "Hey is this thing on!"
+//   action_slack_channel       = "@dennis"
+//   action_slack_message       = "Hey dude!"
+//   alert_comparator           = "greater than"
+//   alert_threshold            = "1"
+//   dispatch_earliest_time     = "-15m"
+//   cron_schedule              = "5 5 5 5 *"
+//   is_scheduled               = true
+// }
+// 
+// resource "splunk_saved_search" "test3" {
+//   name                = "Sisyphus Kubectl Errors"
+//   search              = "I need to build this"
+//   action_email_inline = true
+// }
+// 
+// resource "splunk_saved_search" "test4" {
+//   name                = "Sisyphus Kubectl "
+//   search              = "I need to build this"
+//   action_email_inline = true
+// }
+
